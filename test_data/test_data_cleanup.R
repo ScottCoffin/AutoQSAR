7ae7b2834e6b2ca9e0340cd9806bee7d5e692489
Scott Coffin, Ph.D. <56366258+ScottCoffin@users.noreply.github.com>
2026-04-03 14:38:26 -0700

    added test data

---
+++ test_data/test_data_cleanup.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+# import test data from QACs_OEHHA repo
+starting_data <- read_csv("test_data/comprehensive_dataset.csv")
+
+# isolate repro/dev as target and QSAR_Ready_smiles
+repro_dev_data <- starting_data %>%
+  select(QSAR_READY_SMILES, target = `Repro/Dev`) |> 
+  drop_na()
+
+#save csv for processing in autoQSAR
+write_csv(repro_dev_data, "test_data/repro_dev_data.csv")
